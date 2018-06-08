51ec96a297bff1756619057329aaf821cdd35b26
Joseph Kim <josephikim2@gmail.com>
2018-06-08 09:22:55 -0700

    base files

---
+++ models/dog.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+  var Dog = sequelize.define("Dog", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    breed: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    age_years: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        len: [1]
+      }
+    }
+    age_months: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  return Dog;
+};
---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS dogs;
+-- Creates the "blogger" database --
+CREATE DATABASE dogs;
