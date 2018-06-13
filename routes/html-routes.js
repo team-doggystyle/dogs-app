459485bab8934e9f96a1c86e4f869c975f2566ee
Kendra Weinisch <kendraweinisch@gmail.com>
2018-06-12 20:54:23 -0700

    finished html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,6 @@ var path = require("path");
 // Routes
 // =============================================================
 router.get("/", function(req, res) {
-   // res.send('You\'ve reached the homepage');
    res.render("index");
 });
 
@@ -34,19 +33,10 @@ router.get("/dogs", function(req, res) {
 router.get("/dogs/addnewdog", function(req, res) {
   res.render("addnewdog");
 });
-// router.post("/dogs/addnewdog", function(req, res) {
-//   // dog.all(function(data) {
-//   //   var hbsObject = {
-//   //     dogs: data
-//   //   };
-//   //   console.log(hbsObject);
-//   //   res.render("index", hbsObject);
-//   // });
-// });
+
 router.get("/adopt", function(req, res) {
   res.render("adopt");
 });
-
 
 router.get("/application", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/assets/application.html"));
@@ -55,28 +45,10 @@ router.get("/application", function(req, res) {
 // router.post("/adopt/application", function(req, res) {
 // });
 
-// router.get("/help", function(req, res) {
-// });
+router.get("/help", function(req, res) {
+      res.sendFile(path.join(__dirname, "../help.html"));
+});
 // router.post("/help/donate", function(req, res) {
 
 // });
  module.exports = router;
-// module.exports = function(app) {
-
-//   // Each of the below routes just handles the HTML page that the user gets sent to.
-
-//   // index route loads view.html
-//   app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/blog.html"));
-//   });
-
-//   app.get("/cms", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/cms.html"));
-//   });
-
-//   // blog route loads blog.html
-//   app.get("/blog", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/blog.html"));
-//   });
-
-// };
