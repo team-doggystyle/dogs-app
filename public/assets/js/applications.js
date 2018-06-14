e67e3c6ec9a3ef31dfc2bfc66e129b12cbd62615
AnutaK <akvaisman@gmail.com>
2018-06-13 20:04:34 -0700

    A new application record is now being logged into the database

--- public/assets/js/applications.js
+++ public/assets/js/applications.js
@@ -18,43 +18,4 @@ $(document).ready(function () {
     
     })
 
-    // Getting jQuery references to the post body, title, form, and category select
-    
-    // Giving the inputPhone a default value
-    // inputPhone.val("Personal");
-
-    
-    // // Submits a new post and brings user to blog page upon completion
-    // function submitPost(Post) {
-    //     $.post("/api/posts/", Post, function () {
-    //         window.location.href = "/blog";
-    //     });
-    // }
-
-    // // Gets post data for a post if we're editing
-    // function getApplicationData(id) {
-    //     $.get("/api/posts/" + id, function (data) {
-    //         if (data) {
-    //             // If this post exists, prefill our cms forms with its data
-    //             lastName.val(data.title);
-    //             firstName.val(data.body);
-    //             inputPhone.val(data.category);
-    //             // If we have a post with this id, set a flag for us to know to update the post
-    //             // when we hit submit
-    //             updating = true;
-    //         }
-    //     });
-    // }
-
-    // Update a given post, bring user to the blog page when done
-    // function updatePost(post) {
-    //     $.ajax({
-    //             method: "PUT",
-    //             url: "/api/posts",
-    //             data: post
-    //         })
-    //         .then(function () {
-    //             window.location.href = "/blog";
-    //         });
-    // }
 });
